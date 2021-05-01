1be6c5cd07afe748399ed5f832e3ea05ae381198
Swan Pham <accounting@nailsrus.ca>
2021-05-01 09:55:50 -0400

    generated HTML

---
+++ index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name), err => {
+    if (err) throw err;
+  
+    console.log('Team Profile! Check out index.html to see the output!');
+});    
+
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+
+const Engineer = require('./lib/Engineer');
+
+const Intern = require('./lib/Intern');
+
+let employees = [];
+
+function questions(roles) {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Enter employee role',
+            default: roles[0],
+            choices: roles        
+        },
+        {
+            type:'text',
+            name:'name',
+            message:'enter name'
+        },
+
+        {
+            type:'text',
+            name:'id',
+            message:'enter id'
+        },
+
+        {
+            type:'text',
+            name:'email',
+            message:'enter email'
+        },
+
+        {
+            type:'text',
+            name:'office',
+            message:'enter office numbers',
+            when: ({role}) => {
+                if (role === 'Manager') {
+                    return true;
+                }else{
+                    return false;
+                }
+            },
+        },
+
+        {
+            type:'text',
+            name:'github',
+            message:'enter github username',
+            when: ({role}) => {
+                if (role === 'Engineer') {
+                    return true;
+                }else{
+                    return false;
+                }
+            },
+        },
+
+        {
+            type:'text',
+            name:'school',
+            message:'enter school name',
+            when: ({role}) => {
+                if (role === 'Intern') {
+                    return true;
+                }else{
+                    return false;
+                }
+            },
+        }, 
+
+        {
+            type:'confirm',
+            name:'confirmNews',
+            message:'Do you want to add more Employee?',
+            default: false
+        }
+
+        
+]).then (data => {
+    employees.push(data)
+    if (data.confirmNews === true) {
+        console.log ('---------------');
+        return questions (['Engineer','Intern']);
+    }else {
+        console.log (employees);
+    }
+})
+}
+questions(['Manager']);
+
+
+
---
+++ src/page-template.js
@@ -0,0 +1,19 @@
+const generatePage = (name) => {
+  return `
+  <!DOCTYPE html> 
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team Profile </title>
+</head>
+
+<body>
+  <h1>${name}</h1>
+</body>
+</html>
+`;
+};
+
+module.exports = generatePage;
